eac5f9c04c2c658b0eb2af00e94ed67dc5a0286f
Ricardo <rick96_@hotmail.com>
2018-12-02 21:24:50 -0600

    se agrego el componente Book

--- cliente/src/router/index.js
+++ cliente/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Ping from '@/components/Ping';
+import Book from '@/components/Book';
 
 Vue.use(Router);
 
@@ -8,8 +9,14 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Book',
+      component: Book,
+    },
+    {
+      path: '/ping',
       name: 'Ping',
       component: Ping,
     },
   ],
+  mode: 'hash',
 });
